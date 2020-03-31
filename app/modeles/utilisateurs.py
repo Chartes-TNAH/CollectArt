df04724846ce0f94d2037d6eeb6d3b1bb58c2f46
carolinecorbieres <58423346+carolinecorbieres@users.noreply.github.com>
2020-03-31 15:28:55 +0200

    Ajout de commentaires et corrections

--- app/modeles/utilisateurs.py
+++ app/modeles/utilisateurs.py
@@ -1,6 +1,5 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from sqlalchemy import or_
 
 from .. app import db, login
 
@@ -20,18 +19,10 @@ class User(UserMixin, db.Model):
         :returns: ID de l'utilisateur·rice
         :rtype: int
         """
-        return(self.user_id)
-
-    @login.user_loader
-    def trouver_utilisateur_via_id(user_id):
-        """
-        Permet de récupérer un·e utilisateur·rice en fonction de son identifiant
-        :returns: ID de l'utilisateur·rice
-        :rtype: int
-        """
-        return User.query.get(int(user_id)) 
+        return(self.user_id) 
 
     @staticmethod
+    # @staticmethod permet d'appeler la fonction de la manière suivante : user = User.identification(login, password)
     def identification(login, password):
         """ 
         Identifie un utilisateur·rice. Si cela fonctionne, renvoie les données de l'utilisateur·rice.
@@ -54,25 +45,25 @@ class User(UserMixin, db.Model):
         :param login: Login de l'utilisateur·rice
         :param email: Email de l'utilisateur·rice
         :param nom: Nom de l'utilisateur·rice
-        :param motdepasse: Mot de passe de l'utilisateur·rice (Minimum 6 caractères)
+        :param password: Mot de passe de l'utilisateur·rice (Minimum 6 caractères)
         :return: tuple
         """
         errors = []
         if not login:
-            errors.append("Le login fourni est vide")
+            errors.append("le login fourni est vide")
         if not email:
-            errors.append("L'email fourni est vide")
+            errors.append("l'email fourni est vide")
         if not name:
-            errors.append("Le nom fourni est vide")
+            errors.append("le nom fourni est vide")
         if not password or len(password) < 6:
-            errors.append("Le mot de passe fourni est vide ou trop court")
+            errors.append("le mot de passe fourni est vide ou trop court")
 
         # On vérifie que personne n'a utilisé cet email ou ce login
-        uniques = User.query.filter(or_
+        uniques = User.query.filter(db.or_
             (User.user_email == email, User.user_login == login)
         ).count()
         if uniques > 0:
-            errors.append("L'email ou le login sont déjà utilisés")
+            errors.append("l'email ou le login sont déjà utilisés")
 
         # Si on a au moins une erreur
         if len(errors) > 0:
@@ -94,6 +85,16 @@ class User(UserMixin, db.Model):
 
             # On renvoie l'utilisateur·rice
             return True, user
-        
-        except Exception as erreur:
-            return False, [str(erreur)]
+
+        except Exception as error:
+            return False, [str(error)]
+
+
+    @login.user_loader
+    def trouver_utilisateur_via_id(user_id):
+        """
+        Permet de récupérer un·e utilisateur·rice en fonction de son identifiant
+        :returns: ID de l'utilisateur·rice
+        :rtype: int
+        """
+        return User.query.get(int(user_id))
