9584e116f8cc3f6b29d05604117001f66cbb0b03
carolinecorbieres <58423346+carolinecorbieres@users.noreply.github.com>
2020-04-06 18:46:39 +0200

    Ajout de commentaires

--- app/modeles/donnees.py
+++ app/modeles/donnees.py
@@ -1,6 +1,8 @@
 from flask import url_for
+# importation de url_for (pour construire des URL vers les fonctions et les pages html) depuis flask
 
 from .. app import db
+# importation de la BDD
 
 
 class Collection(db.Model):
@@ -14,15 +16,13 @@ class Collection(db.Model):
     authorships_collection = db.relationship("Authorship_collection", back_populates="collection_collection")
     work = db.relationship("Work", backref="collection")
 
-
     def get_id(self):
         """
         Retourne l'id de l'objet actuellement utilisé
-        :returns: ID de l'oeuvre
+        :returns: ID de la collection
         :rtype: int
         """
         return(self.collection_id)
-
 
     @staticmethod
     def add_collection(name, collector_name, collector_firstname, collector_date, collector_bio):
@@ -31,7 +31,7 @@ class Collection(db.Model):
         :param name: nom de la collection (str)
         :param collector_name: nom de famille du/de la collectionneur·euse (str)
         :param collector_firstname: prénom du/de la collectionneur·euse (str)
-        :param collector_date: date(s) du/de la collectionneur·euse (str)
+        :param collector_date: date·s du/de la collectionneur·euse (str)
         :param collector_bio: petite biographie du/de la collectionneur·euse (str)
         :return: Booléen
         """
@@ -46,8 +46,8 @@ class Collection(db.Model):
             errors.append("veuillez renseigner le prénom du/de la collectionneur·euse")
         if not collector_date:
             errors.append("veuillez renseigner les dates du/de la collectionneur·euse, si elles sont inconnues indiquer: dates inconnues")
-        # vérification que les champs sont bien renseignés (des indications dans le message d'erreur permettent 
-        # de compléter les données si elles sont inconues)
+        # vérification que les champs sont bien renseignés (des indications dans le message d'erreur permettent de compléter 
+	# les données si elles sont inconues)
 
         if len(errors) > 0:
             return False, errors
@@ -59,31 +59,29 @@ class Collection(db.Model):
             collection_collector_firstname=collector_firstname,
             collection_collector_date=collector_date,
             collection_collector_bio=collector_bio)
-        # ajout d'une nouvelle entrée collection dans la table collection avec les champs correspondant aux 
-        # paramètres du modèle
+        # ajout d'une nouvelle entrée collection dans la table collection avec les champs correspondant aux paramètres du 
+	# modèle
 
         try:
             db.session.add(new_collection)
+	    # ajout de l'oeuvre à la BDD
             db.session.commit()
-            # ajout de l'oeuvre à la BDD
-
             return True, new_collection
 
         except Exception as erreur:
             return False, [str(erreur)]
 
-
     @staticmethod
     def update_collection(collection_id, name, collector_name, collector_firstname, collector_date, collector_bio):
         """
-        Fonction qui permet d'ajouter une nouvelle collection dans la BDD
+        Fonction qui permet de modifier une collection de la BDD
         :param collection_id: id de la collection (int)
         :param name: nom de la collection (str)
         :param collector_name: nom de famille du/de la collectionneur·euse (str)
         :param collector_firstname: prénom du/de la collectionneur·euse (str)
-        :param collector_date: date(s) du/de la collectionneur·euse (str)
+        :param collector_date: date·s du/de la collectionneur·euse (str)
         :param collector_bio: petite biographie du/de la collectionneur·euse (str)
-        :return:Booléen
+        :return: Booléen
         """
         errors=[]
         if not name:
@@ -122,44 +120,44 @@ class Collection(db.Model):
 
         try:
             db.session.add(update_collection)
+	    # ajout des modifications à la BDD
             db.session.commit()
-        # ajout des modifications à la BDD
             return True, update_collection
 
         except Exception as erreur:
             return False, [str(erreur)]
 
-
     @staticmethod
     def delete_collection(collection_id, name, collector_name, collector_firstname, collector_date, collector_bio):
         """
-        Fonction qui supprime la notice d'une oeuvre
+        Fonction qui supprime une collection
         :param collection_id: id de la collection (int)
         :param name: nom de la collection (str)
         :param collector_name: nom de famille du/de la collectionneur·euse (str)
         :param collector_firstname: prénom du/de la collectionneur·euse (str)
-        :param collector_date: date(s) du/de la collectionneur·euse (str)
+        :param collector_date: date·s du/de la collectionneur·euse (str)
         :param collector_bio: petite biographie du/de la collectionneur·euse (str)
         :returns: Booléen
         """
-        collection = Collection.query.get(collection_id)
+        delete_collection = Collection.query.get(collection_id)
+	# récupération d'une collection dans la BDD
 
-        collection.collection_id = collection_id
-        collection.collection_name = name
-        collection.collection_collector_name = collector_name
-        collection.collection_collector_firstname = collector_firstname
-        collection.collection_collector_date = collector_date
-        collection.collection_collector_bio = collector_bio
+        delete_collection.collection_id = collection_id
+        delete_collection.collection_name = name
+        delete_collection.collection_collector_name = collector_name
+        delete_collection.collection_collector_firstname = collector_firstname
+        delete_collection.collection_collector_date = collector_date
+        delete_collection.collection_collector_bio = collector_bio
+	# suppression des données de la collection
 
         try:
-            db.session.delete(collection)
+            db.session.delete(delete_collection)
+	    # suppression de la collection de la BDD
             db.session.commit()
-            # suppression de la collection de la BDD
-            return True, collection
+            return True, delete_collection
 
         except Exception as erreur:
             return False, [str(erreur)]
-
 
 
 class Work(db.Model):
@@ -204,30 +202,26 @@ class Work(db.Model):
             errors.append("veuillez renseigner la technique de l'oeuvre")
         if not dimensions:
             errors.append("veuillez renseigner les dimensions de l'oeuvre, si elles sont inconnues indiquer: dimensions inconnues")
-            
-        # Si on a au moins une erreur, cela retourne faux
+         
         if len(errors) > 0:
             return False, errors
 
-        # ajout d'une nouvelle entrée oeuvre dans la table work avec les champs correspondant aux paramètres du modèle
-        new_work = Work(work_title=title,
-        	          work_author=author,
-        	          work_date=date,
-        	          work_medium=medium,
-        	          work_dimensions=dimensions,
-                      work_image_lien=image,
-                      work_collection_id=collection_id)
+        new_work = Work(
+		work_title=title,
+		work_author=author,
+		work_date=date,
+		work_medium=medium,
+		work_dimensions=dimensions,
+		work_image_lien=image,
+		work_collection_id=collection_id)
 
         try:
         	db.session.add(new_work)
         	db.session.commit()
-        	# ajout de l'oeuvre à la BDD
-
         	return True, new_work
 
         except Exception as erreur:
         	return False, [str(erreur)]
-
 
     @staticmethod
     def update_work (work_id, title, author, date, medium, dimensions, image):
@@ -253,12 +247,10 @@ class Work(db.Model):
         if not dimensions:
             errors.append("veuillez renseigner les dimensions de l'oeuvre, si elles sont inconnues indiquer: dimensions inconnues")
 
-        # Si on a au moins une erreur, cela retourne faux
         if len(errors) > 0:
             return False, errors
 
         update_work = Work.query.get(work_id)
-        # récupération d'une oeuvre dans la BDD
 
         if update_work.work_title == title \
            and update_work.work_author == author \
@@ -267,7 +259,6 @@ class Work(db.Model):
            and update_work.work_dimensions == dimensions \
            and update_work.work_image_lien == image:
            erreurs.append("Aucune modification n'a été réalisée")
-        # vérification qu'au moins un champ est modifié
 
         if len(errors) > 0:
             return False, errors
@@ -279,63 +270,48 @@ class Work(db.Model):
             update_work.work_medium=medium
             update_work.work_dimensions=dimensions
             update_work.work_image_lien=image
-        # mise à jour de l'oeuvre
 
         try:
             db.session.add(update_work)
             db.session.commit()
-        # ajout des modifications à la BDD
             return True, update_work
 
         except Exception as erreur:
             return False, [str(erreur)]
 
-        errors=[]
-        if not name:
-            errors.append("veuillez renseigner le nom de la collection")
-        if not collector_name:
-            errors.append("veuillez renseigner le nom de famille du/de la collectionneur·euse")
-        if not collector_firstname:
-            errors.append("veuillez renseigner le prénom du/de la collectionneur·euse")
-        if not collector_date:
-            errors.append("veuillez renseigner les dates du/de la collectionneur·euse, si elles sont inconnues indiquer: dates inconnues")
-        if not collector_bio:
-            errors.append("veuillez renseigner une petite biographie du/de la collectionneur·euse")
-
-
     @staticmethod
     def delete_work(work_id, title, author, date, medium, dimensions, image):
         """
-        Fonction qui supprime la notice d'une oeuvre
-        :param work_id: id de l'oeuvre
-        :type work_id: int
+        Fonction qui supprime la notice d'une oeuvre et ses données 
+        :param work_id: id de l'oeuvre (int)
+        :param title: titre de l'oeuvre (str)
+        :param author: nom de l'auteur de l'oeuvre, c'est-à-dire de l'artiste (str)
+        :param date: date de création de l'oeuvre (str)
+        :param medium: 'peinture', 'sculpture', 'gravure', 'dessin', "objet d'art" ou 'photographie'(str)
+        :param dimensions: dimensions de l'oeuvre (str)
         :returns : Booléen
         """
-        work = Work.query.get(work_id)
+        delete_work = Work.query.get(work_id)
 
-        work.work_id = work_id
-        work.work_title = title
-        work.work_author = author
-        work.work_date = date
-        work.work_medium = medium
-        work.work_dimensions = dimensions
-        work.work_image_lien = image
-        # récupération de la notice de l'oeuvre
+        delete_work.work_id = work_id
+        delete_work.work_title = title
+        delete_work.work_author = author
+        delete_work.work_date = date
+        delete_work.work_medium = medium
+        delete_work.work_dimensions = dimensions
+        delete_work.work_image_lien = image
 
         try:
-            db.session.delete(work)
+            db.session.delete(delete_work)
             db.session.commit()
-            # suppression de l'oeuvre de la BDD
-            return True, work
+            return True, delete_work
 
         except Exception as erreur:
             return False, [str(erreur)]
 
 
-
 class Mediums(db.Model):
 	label = db.Column(db.Text, unique=True, nullable=False, primary_key=True)
-
 
 
 class Authorship_collection(db.Model):
@@ -346,8 +322,7 @@ class Authorship_collection(db.Model):
     user_collection = db.relationship("User", back_populates="author_collection")
     collection_collection = db.relationship("Collection", back_populates="authorships_collection")
 
-
-        
+     
 class Authorship_work(db.Model):
     __tablename__ = "authorship_work"
     authorship_work_id = db.Column(db.Integer, nullable=True, autoincrement=True, primary_key=True)
