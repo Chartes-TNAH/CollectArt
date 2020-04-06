f874111bf9bd1d5e01a156ed4f648a612f96ec2f
carolinecorbieres <58423346+carolinecorbieres@users.noreply.github.com>
2020-04-06 15:53:59 +0200

    Ajout de commentaires

--- app/constantes.py
+++ app/constantes.py
@@ -1,6 +1,8 @@
 from warnings import warn
 
 RESULTATS_PAR_PAGE = 6
+# variable qui définit le nombre de résultats par page (utilisée pour l'index et la recherche simple)
+
 SECRET_KEY = "JE SUIS UN SECRET !"
 # variable utilisée comme clé cryptographique, qui permet de générer des sigantures ou tokens
 
