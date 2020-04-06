75993ae63a1beb2203bda1986c446ec0dea39556
carolinecorbieres <58423346+carolinecorbieres@users.noreply.github.com>
2020-04-06 15:27:25 +0200

    Ajout commentaires

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app.app import app
-# importation de la variable app (l'application) depuis le package app
+# importation de la variable app (l'application) depuis le package app (variable définie dans le fichier app.py)
 
 if __name__ == "__main__":
 	app.run(debug=True)
 # app.run() lance l'application
+# le mode debug permet de lancer un débogueur pendant le développement de l'application
