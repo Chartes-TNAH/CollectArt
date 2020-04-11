48b8777decf0e5ddc8fd87e58c9dd1ee0483256f
carolinecorbieres <58423346+carolinecorbieres@users.noreply.github.com>
2020-04-11 12:24:40 +0200

    Ajout des classes test et production

--- app/constantes.py
+++ app/constantes.py
@@ -10,3 +10,26 @@ if SECRET_KEY == "JE SUIS UN SECRET !":
     warn("Le secret par défaut n'a pas été changé, vous devriez le faire", Warning)
 
 
+class _TEST:
+	# comme la classe débute par _, cela signifie qu'elle ne devrait pas être appelée directement
+    SECRET_KEY = SECRET_KEY
+    # configuration du secret
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test_db.sqlite'
+    # configuration de la base de données test
+    # chemin qui indique où chercher la BDD: chemin absolu vers le fichier db.sqlite
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class _PRODUCTION:
+    SECRET_KEY = SECRET_KEY
+    # configuration du secret
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../db.sqlite'
+    # configuration de la base de données production
+    # chemin qui indique où chercher la BDD: chemin absolu vers le fichier db.sqlite
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+CONFIG = {
+    "test": _TEST,
+    "production": _PRODUCTION
+    # les deux classes sont regroupées dans un dictionnaire afin de pouvoir les appeler facilement
+}
