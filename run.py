035e86cfef7ad5a39c6329632c3152abae299320
carolinecorbieres <58423346+carolinecorbieres@users.noreply.github.com>
2020-04-11 12:22:57 +0200

    Ajout de la fonction config_app (test/prod)

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-from app.app import app
-# importation de la variable app (l'application) depuis le package app (variable définie dans le fichier app.py)
+from app.app import config_app
+# importation de la focntion config_app (qui retourne l'application) depuis le package app (variable définie dans le 
+# fichier app.py)
 
 if __name__ == "__main__":
+	app = config_app("production")
 	app.run(debug=True)
 # app.run() lance l'application
 # le mode debug permet de lancer un débogueur pendant le développement de l'application
